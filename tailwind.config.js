4f8fd325a550e38f0dd8859647bddaf4f8a37851
vivalorancio <vmateu@gmail.com>
2020-12-31 18:23:12 +0100

    second

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,22 +8,16 @@ module.exports = {
       blanc: {
         light: '#f5fcf3',
         DEFAULT: '#f1faee',
-        //dark: '#009eeb',
       },
       roig: {
-        // light: '#85d7ff',
         DEFAULT: '#aa1e22',
         dark: '#741417',
       },
       blaucel: {
-        // light: '#85d7ff',
         DEFAULT: '#457b9d',
-        //dark: '#009eeb',
       },
       blaumari: {
-        // light: '#85d7ff',
         DEFAULT: '#1d3557',
-        //dark: '#009eeb',
       },
     },
     fontFamily: {
